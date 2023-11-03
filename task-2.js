1a2dca6bf1bf34ab6865d77d1f23d768081425de
BaseOfExistence <139703947+BaseOfExistence@users.noreply.github.com>
2023-11-03 20:13:32 +0200

    Initial commit

---
+++ task-2.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function makeArray(firstArray = [], secondArray = [], maxLength = 0) {
+    const newArray = firstArray.concat(secondArray);
+    for (; newArray.length > maxLength;) {
+        newArray.pop();
+    }
+    return newArray;
+}
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
